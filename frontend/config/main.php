3410e55bf90f91f367282e2426f4afad6f35b800
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-02 13:45:24 +0300

    Add rule for blog controller.

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -43,6 +43,10 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'blog/blog'
+                ],
             ],
         ],
     ],
