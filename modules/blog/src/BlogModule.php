2e9b9e991910ad7b87a4a5526199468323a64f03
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-02 15:02:33 +0300

    Add migration to app migrations pool.

--- modules/blog/src/BlogModule.php
+++ modules/blog/src/BlogModule.php
@@ -2,13 +2,16 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Blog;
 
+use Yii;
 use yii\base\Application AS BaseApp;
 use yii\web\Application AS WebApp;
+use yii\console\Application AS ConsoleApp;
 
 class BlogModule extends \yii\base\Module
 {
     public function init()
     {
+        Yii::setAlias('@' . str_replace('\\', '/', __NAMESPACE__), __DIR__);
         /** @var BaseApp $app */
         $app = $this->module;
         $this->registerControllers($app);
@@ -18,6 +21,15 @@ class BlogModule extends \yii\base\Module
     {
         if ($app instanceof WebApp && $app->id = 'app-frontend') {
             $this->controllerNamespace = __NAMESPACE__ . '\controllers\frontend';
+        }
+        if ($app instanceof ConsoleApp) {
+            if (!array_key_exists('migrate', $app->controllerMap)) {
+                $app->controllerMap['migrate'] = [
+                    'class' => 'yii\console\controllers\MigrateController',
+                    'migrationNamespaces' => [],
+                ];
+            }
+            $app->controllerMap['migrate']['migrationNamespaces'][] = __NAMESPACE__ . '\migrations';
         }
     }
 }
