76a6967838bda5d3f0a6756fd9ea8992aae81327
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-03 12:12:44 +0300

    Create UserApiKeyEntity

---
+++ modules/auth/src/entities/UserApiKeyEntity.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Auth\entities;
+
+use yii\db\ActiveQuery;
+use common\models\User;
+
+/**
+ * This is the model class for table "{{%auth_user_api_keys}}".
+ *
+ * @property int $user_id
+ * @property string $api_key
+ *
+ * @property User $user
+ */
+class UserApiKeyEntity extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%auth_user_api_keys}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['api_key'], 'required'],
+            [['api_key'], 'string', 'max' => 255],
+            [
+                ['user_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => User::class,
+                'targetAttribute' => ['user_id' => 'id']
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'api_key' => 'Api Key',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+}
