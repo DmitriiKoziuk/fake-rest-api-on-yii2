7f30c3b533a5fa8135746ad869a33509b64df6ac
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-03 13:01:53 +0300

    Implement findIdentityByAccessToken method

--- modules/auth/src/entities/User.php
+++ modules/auth/src/entities/User.php
@@ -4,4 +4,15 @@ namespace DmitriiKoziuk\FakeRestApiModules\Auth\entities;
 
 class User extends \common\models\User
 {
+    public static function findIdentityByAccessToken($token, $type = null): ?\common\models\User
+    {
+        /** @var UserApiKeyEntity $userApiKeyEntity */
+        $userApiKeyEntity = UserApiKeyEntity::find()->where([
+            'api_key' => $token,
+        ])->one();
+        if (! empty($userApiKeyEntity)) {
+            return $userApiKeyEntity->user;
+        }
+        return null;
+    }
 }
