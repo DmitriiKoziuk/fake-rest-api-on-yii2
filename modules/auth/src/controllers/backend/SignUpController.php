12ac8fb7458a5227b5bbf33245c9682f964aa352
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-05 16:36:07 +0300

    Write SignUpController::actionCreate() for incoming POST request. Return error if incoming POST request empty or not valid.

--- modules/auth/src/controllers/backend/SignUpController.php
+++ modules/auth/src/controllers/backend/SignUpController.php
@@ -2,7 +2,10 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Auth\controllers\backend;
 
+use Yii;
 use yii\rest\Controller;
+use DmitriiKoziuk\FakeRestApiModules\Auth\forms\UserSignUpForm;
+use DmitriiKoziuk\FakeRestApiModules\Auth\exceptions\forms\UserSignUpFormNotValidException;
 
 class SignUpController extends Controller
 {
