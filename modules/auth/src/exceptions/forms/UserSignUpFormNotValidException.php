48cda9daf8f5e6592914f36df31de32b9efb2026
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-05 16:45:41 +0300

    UserSignUpFormNotValidException require errors from UserSignUpFor

--- modules/auth/src/exceptions/forms/UserSignUpFormNotValidException.php
+++ modules/auth/src/exceptions/forms/UserSignUpFormNotValidException.php
@@ -5,4 +5,16 @@ namespace DmitriiKoziuk\FakeRestApiModules\Auth\exceptions\forms;
 class UserSignUpFormNotValidException extends \Exception
 {
     protected $message = 'User sign up form not valid.';
+    protected array $formValidationErrors;
+
+    public function __construct(array $formValidationErrors)
+    {
+        parent::__construct($this->message, 0, null);
+        $this->formValidationErrors = $formValidationErrors;
+    }
+
+    public function getValidationErrors(): array
+    {
+        return $this->formValidationErrors;
+    }
 }
