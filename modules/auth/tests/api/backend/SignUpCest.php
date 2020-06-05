9c97b0c0866b22997c9bb3870f5757f1c0394fa9
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-05 16:09:36 +0300

    Write test to check is sign-in resource work.

--- modules/auth/tests/api/backend/SignUpCest.php
+++ modules/auth/tests/api/backend/SignUpCest.php
@@ -2,8 +2,15 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Auth\tests\api\backend;
 
+use yii\helpers\Url;
 use DmitriiKoziuk\FakeRestApiModules\Auth\tests\ApiTester;
 
 class SignUpCest
 {
+    public function tryToCheckIsSignInResourceWork(ApiTester $I)
+    {
+        $I->sendGET(Url::to(['/auth/sign-up']));
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseContains('Hello');
+    }
 }
