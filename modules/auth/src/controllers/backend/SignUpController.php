bc3b5abbeac2fef3f6549953b172d8caa68131e9
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-05 16:36:54 +0300

    Write SignUpController::actionCreate() for incoming POST request. Return error if incoming POST request empty or not valid.

--- modules/auth/src/controllers/backend/SignUpController.php
+++ modules/auth/src/controllers/backend/SignUpController.php
@@ -13,4 +13,27 @@ class SignUpController extends Controller
     {
         return 'Hello, SignUp';
     }
+
+    public function actionCreate(): array
+    {
+        try {
+            $return = [
+                'success' => false,
+                'statusMessage' => '',
+                'data' => [],
+            ];
+            $userSignUpForm = new UserSignUpForm();
+            if (!
+                $userSignUpForm->load(Yii::$app->request->post()) &&
+                ! $userSignUpForm->validate()
+            ) {
+                throw new UserSignUpFormNotValidException();
+            }
+        } catch (UserSignUpFormNotValidException $e) {
+            $return['statusMessage'] = $e->getMessage();
+        } catch (\Throwable $e) {
+            $return['statusMessage'] = 'Internal application error.';
+        }
+        return $return;
+    }
 }
