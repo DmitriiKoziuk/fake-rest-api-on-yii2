8b0ae0fefb33cc6d516aef57d647f79678353822
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 15:09:38 +0300

    Create skeleton test for PostService

---
+++ modules/blog/tests/unit/services/PostServiceTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\unit\services;
+
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\UnitTester;
+
+class PostServiceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+}
