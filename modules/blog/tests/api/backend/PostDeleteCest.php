85e86bff3bf4a4082e608b5032294a0ccbfb31ec
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 17:39:43 +0300

    Add test "tryToDeletePost".

--- modules/blog/tests/api/backend/PostDeleteCest.php
+++ modules/blog/tests/api/backend/PostDeleteCest.php
@@ -2,10 +2,13 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\api\backend;
 
+use yii\helpers\Url;
 use DmitriiKoziuk\FakeRestApiModules\Blog\tests\ApiTester;
 use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserApiKeyEntityFixture;
 use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserEntityFixture;
+use DmitriiKoziuk\FakeRestApiModules\Auth\entities\UserApiKeyEntity;
 use DmitriiKoziuk\FakeRestApiModules\Blog\tests\_fixtures\PostFixture;
+use DmitriiKoziuk\FakeRestApiModules\Blog\entities\Post;
 
 class PostDeleteCest
 {
@@ -16,5 +19,18 @@ class PostDeleteCest
             'apiKeys' => UserApiKeyEntityFixture::class,
             'posts' => PostFixture::class,
         ];
+    }
+
+    public function tryToDeletePost(ApiTester $I)
+    {
+        /** @var Post $postEntity */
+        $postEntity = $I->grabFixture('posts', 0);
+        /** @var UserApiKeyEntity $apiKeyEntity */
+        $apiKeyEntity = $I->grabFixture('apiKeys', 0);
+        $url = '/blog/posts/' . $postEntity->id;
+
+        $I->amBearerAuthenticated($apiKeyEntity->api_key);
+        $I->sendDELETE(Url::to([$url]));
+        $I->seeResponseCodeIs(204);
     }
 }
