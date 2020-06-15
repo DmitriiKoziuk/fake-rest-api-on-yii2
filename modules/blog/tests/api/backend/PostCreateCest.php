3b0749ad441c9355f841c56bd287e7e58bf5fe3e
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 17:09:12 +0300

    Add api test "tryToCreateNewPost".

--- modules/blog/tests/api/backend/PostCreateCest.php
+++ modules/blog/tests/api/backend/PostCreateCest.php
@@ -2,9 +2,12 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\api\backend;
 
+use yii\helpers\Url;
 use DmitriiKoziuk\FakeRestApiModules\Blog\tests\ApiTester;
 use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserApiKeyEntityFixture;
 use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserEntityFixture;
+use DmitriiKoziuk\FakeRestApiModules\Auth\entities\User;
+use DmitriiKoziuk\FakeRestApiModules\Auth\entities\UserApiKeyEntity;
 
 class PostCreateCest
 {
@@ -14,5 +17,28 @@ class PostCreateCest
             'users' => UserEntityFixture::class,
             'apiKeys' => UserApiKeyEntityFixture::class,
         ];
+    }
+
+    public function tryToCreateNewPost(ApiTester $I)
+    {
+        /** @var User $userEntity */
+        $userEntity = $I->grabFixture('users', 0);
+        /** @var UserApiKeyEntity $userApiKeyEntity */
+        $userApiKeyEntity = $I->grabRecord(UserApiKeyEntity::class, ['user_id' => $userEntity->id]);
+        $postTitle = 'New post title.';
+        $postBody = 'New post body.';
+
+        $I->amBearerAuthenticated($userApiKeyEntity->api_key);
+        $I->sendPOST(Url::to(['/blog/posts']), [
+            'title' => $postTitle,
+            'body' => $postBody,
+        ]);
+
+        $I->seeResponseCodeIs(201);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            'title' => $postTitle,
+            'body' => $postBody,
+        ]);
     }
 }
