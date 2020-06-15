828656a1ebff9771c40a32a022bd402e2513afab
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 20:44:11 +0300

    Implement method findPosts()

--- modules/blog/src/repositories/PostRepository.php
+++ modules/blog/src/repositories/PostRepository.php
@@ -2,6 +2,18 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Blog\repositories;
 
+use DmitriiKoziuk\FakeRestApiModules\Blog\entities\Post;
+use DmitriiKoziuk\FakeRestApiModules\Blog\forms\PostSearchForm;
+
 class PostRepository
 {
+    public function findPosts(PostSearchForm $postSearchForm): array
+    {
+        $q = Post::find()
+            ->andFilterWhere(['like', 'title', $postSearchForm->title]);
+        return [
+            'totalItems' => $q->count(),
+            'results' => $q->all(),
+        ];
+    }
 }
