b6d9a71dffec342a6ab1aae56bbf46eaf71d482b
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 15:13:17 +0300

    Create skeleton for test PostCreateForm

---
+++ modules/blog/tests/unit/forms/PostCreateFormTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\unit\forms;
+
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\UnitTester;
+
+class PostCreateFormTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+}
