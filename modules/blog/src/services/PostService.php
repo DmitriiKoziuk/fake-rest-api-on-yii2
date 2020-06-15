4eceaba3788d9dd86ade5904e94abf432777135b
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 15:55:51 +0300

    PostService::createPost() return created post data.

--- modules/blog/src/services/PostService.php
+++ modules/blog/src/services/PostService.php
@@ -2,15 +2,22 @@
 
 namespace DmitriiKoziuk\FakeRestApiModules\Blog\services;
 
+use DmitriiKoziuk\FakeRestApiModules\Blog\entities\Post;
 use DmitriiKoziuk\FakeRestApiModules\Blog\forms\PostCreateForm;
 use DmitriiKoziuk\FakeRestApiModules\Blog\exceptions\PostCreateFormNotValidException;
 
 class PostService
 {
-    public function createPost(PostCreateForm $postCreateForm)
+    public function createPost(PostCreateForm $postCreateForm): array
     {
         if (! $postCreateForm->validate()) {
             throw new PostCreateFormNotValidException();
         }
+        $newPost = new Post([
+            'title' => $postCreateForm->title,
+            'body' => $postCreateForm->body,
+        ]);
+        $newPost->save();
+        return $newPost->getAttributes();
     }
 }
