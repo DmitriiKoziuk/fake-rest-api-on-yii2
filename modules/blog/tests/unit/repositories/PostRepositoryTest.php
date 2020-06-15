e05a95d506df7eb678dfc17338e543408c542f24
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 19:53:17 +0300

    Create skeleton for PostRepositoryTest

---
+++ modules/blog/tests/unit/repositories/PostRepositoryTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\unit\repositories;
+
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\UnitTester;
+
+class PostRepositoryTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+}
