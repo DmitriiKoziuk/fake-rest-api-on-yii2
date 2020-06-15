a813b2ad0553b6839153d05f5184f38c17c2fa28
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 16:40:45 +0300

    Add test "testWithNotValidData"

--- modules/blog/tests/unit/entities/PostEntityTest.php
+++ modules/blog/tests/unit/entities/PostEntityTest.php
@@ -20,4 +20,49 @@ class PostEntityTest extends \Codeception\Test\Unit
         ]);
         $this->assertTrue($entity->validate());
     }
+
+    /**
+     * @param string $title
+     * @param string $body
+     * @param array  $invalidFields
+     * @dataProvider notValidDataDataProvider
+     */
+    public function testWithNotValidData(string $title, string $body, array $invalidFields)
+    {
+        $form = new Post([
+            'title' => $title,
+            'body' => $body,
+        ]);
+        $this->assertFalse($form->validate());
+        $errors = $form->getErrors();
+        $this->assertNotEmpty($errors);
+        foreach ($invalidFields as $key => $errorMsg) {
+            $this->assertArrayHasKey($key, $errors);
+            if (! empty($errorMsg)) {
+                $this->assertContains($errorMsg, $errors[ $key ]);
+            }
+        }
+    }
+
+    public function notValidDataDataProvider()
+    {
+        return [
+            'All fields empty' => [
+                'title' => '',
+                'body' => '',
+                'invalidFields' => [
+                    'title' => 'Title cannot be blank.',
+                    'body' => 'Body cannot be blank.',
+                ],
+            ],
+            'Too long title' => [
+                'title' => str_repeat('t', 256),
+                'body' => '',
+                'invalidFields' => [
+                    'title' => 'Title should contain at most 255 characters.',
+                    'body' => '',
+                ],
+            ],
+        ];
+    }
 }
