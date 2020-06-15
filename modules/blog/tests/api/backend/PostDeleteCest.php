6213bbcfdcb5decef63a5f49befccc903d6aa4c7
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 17:29:08 +0300

    Add skeleton for PostDeleteCest

---
+++ modules/blog/tests/api/backend/PostDeleteCest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\api\backend;
+
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\ApiTester;
+use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserApiKeyEntityFixture;
+use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserEntityFixture;
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\_fixtures\PostFixture;
+
+class PostDeleteCest
+{
+    public function _fixtures()
+    {
+        return [
+            'users' => UserEntityFixture::class,
+            'apiKeys' => UserApiKeyEntityFixture::class,
+            'posts' => PostFixture::class,
+        ];
+    }
+}
