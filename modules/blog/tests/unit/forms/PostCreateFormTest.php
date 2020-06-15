651d6003c50cc3c461fdbf83947d1e9baa2e0cf4
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 15:23:39 +0300

    Add test 'testWithNotValidData' for PostCreateForm

--- modules/blog/tests/unit/forms/PostCreateFormTest.php
+++ modules/blog/tests/unit/forms/PostCreateFormTest.php
@@ -20,4 +20,32 @@ class PostCreateFormTest extends \Codeception\Test\Unit
         ]);
         $this->assertTrue($form->validate());
     }
+
+    /**
+     * @param string $title
+     * @param string $body
+     * @dataProvider notValidDataDataProvider
+     */
+    public function testWithNotValidData(string $title, string $body)
+    {
+        $form = new PostCreateForm([
+            'title' => $title,
+            'body' => $body,
+        ]);
+        $this->assertFalse($form->validate());
+    }
+
+    public function notValidDataDataProvider()
+    {
+        return [
+            'All fields empty' => [
+                'title' => '',
+                'body' => '',
+            ],
+            'Too long title' => [
+                'title' => str_repeat('t', 256),
+                'body' => '',
+            ],
+        ];
+    }
 }
