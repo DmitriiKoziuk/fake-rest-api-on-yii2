b1359ffdbfc3738980cd886a9502c9866bacb21a
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 16:42:27 +0300

    Create skeleton PostCreateCest

---
+++ modules/blog/tests/api/backend/PostCreateCest.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\api\backend;
+
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\ApiTester;
+
+class PostCreateCest
+{
+}
