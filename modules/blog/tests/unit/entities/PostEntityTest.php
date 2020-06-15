d61e24a7b75199ec693aa0fb02f33cd2ddb0be58
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 16:37:36 +0300

    Create PostEntityTest skeleton

---
+++ modules/blog/tests/unit/entities/PostEntityTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\unit\entities;
+
+use DmitriiKoziuk\FakeRestApiModules\Blog\tests\UnitTester;
+
+class PostEntityTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+}
