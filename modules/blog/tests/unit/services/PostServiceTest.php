4fe00111f361745a843e1e1284fa7f59ea76f309
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 15:10:27 +0300

    Add fixtures to PostServiceTest

--- modules/blog/tests/unit/services/PostServiceTest.php
+++ modules/blog/tests/unit/services/PostServiceTest.php
@@ -3,6 +3,8 @@
 namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\unit\services;
 
 use DmitriiKoziuk\FakeRestApiModules\Blog\tests\UnitTester;
+use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserApiKeyEntityFixture;
+use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserEntityFixture;
 
 class PostServiceTest extends \Codeception\Test\Unit
 {
@@ -10,4 +12,12 @@ class PostServiceTest extends \Codeception\Test\Unit
      * @var UnitTester
      */
     protected $tester;
+
+    public function _fixtures()
+    {
+        return [
+            'users' => UserEntityFixture::class,
+            'apiKeys' => UserApiKeyEntityFixture::class,
+        ];
+    }
 }
