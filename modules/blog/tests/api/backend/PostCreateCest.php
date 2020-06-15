cffc547b0479154e698a0245f33d62b8179b3f95
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-15 16:43:38 +0300

    Add fixtures

--- modules/blog/tests/api/backend/PostCreateCest.php
+++ modules/blog/tests/api/backend/PostCreateCest.php
@@ -3,7 +3,16 @@
 namespace DmitriiKoziuk\FakeRestApiModules\Blog\tests\api\backend;
 
 use DmitriiKoziuk\FakeRestApiModules\Blog\tests\ApiTester;
+use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserApiKeyEntityFixture;
+use DmitriiKoziuk\FakeRestApiModules\Auth\tests\_fixtures\UserEntityFixture;
 
 class PostCreateCest
 {
+    public function _fixtures()
+    {
+        return [
+            'users' => UserEntityFixture::class,
+            'apiKeys' => UserApiKeyEntityFixture::class,
+        ];
+    }
 }
