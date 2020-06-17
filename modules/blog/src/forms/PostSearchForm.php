dfa603b855b9be99fe54790c56d9c8b301452e79
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-17 11:38:57 +0300

    PostSearchForm. Add dield "resultsPerPage".

--- modules/blog/src/forms/PostSearchForm.php
+++ modules/blog/src/forms/PostSearchForm.php
@@ -9,13 +9,14 @@ class PostSearchForm extends Model
     public ?string $title = null;
     public ?string $body = null;
     public int     $page = 1;
+    public int     $resultsPerPage = 20;
 
     public function rules()
     {
         return [
             [['title'], 'string', 'max' => 255],
             [['body'], 'string'],
-            [['page'], 'integer'],
+            [['page', 'resultsPerPage'], 'integer'],
         ];
     }
 }
