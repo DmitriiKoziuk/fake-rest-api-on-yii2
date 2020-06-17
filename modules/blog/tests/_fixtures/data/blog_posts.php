3b0cda7ada040ef0e7fd85e9e128d616040a7567
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2020-06-17 11:44:13 +0300

    Add more post fixture.

--- modules/blog/tests/_fixtures/data/blog_posts.php
+++ modules/blog/tests/_fixtures/data/blog_posts.php
@@ -11,4 +11,44 @@ return [
         'title' => 'Second post title',
         'body' => 'Second post body',
     ],
+    [
+        'id' => 3,
+        'title' => 'Third post title',
+        'body' => 'Third post body',
+    ],
+    [
+        'id' => 4,
+        'title' => 'Fourth post title',
+        'body' => 'Fourth post body',
+    ],
+    [
+        'id' => 5,
+        'title' => 'Fifth post title',
+        'body' => 'Fifth post body',
+    ],
+    [
+        'id' => 6,
+        'title' => 'Sixth post title',
+        'body' => 'Sixth post body',
+    ],
+    [
+        'id' => 7,
+        'title' => 'Seventh post title',
+        'body' => 'Seventh post body',
+    ],
+    [
+        'id' => 8,
+        'title' => 'Eighth post title',
+        'body' => 'Eighth post body',
+    ],
+    [
+        'id' => 9,
+        'title' => 'Ninth post title',
+        'body' => 'Ninth post body',
+    ],
+    [
+        'id' => 10,
+        'title' => 'Tenth post title',
+        'body' => 'Tenth post body',
+    ],
 ];
